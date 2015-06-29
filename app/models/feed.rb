d430b0ed78c35bd279c74b2aa208448c3a6bfe9c
Aleks <am@klaut.org>
2015-06-29 22:06:58 +0200

    Delete Feed Items after Feed deletion.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,7 +1,7 @@
 class Feed < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  has_many :feed_items
+  has_many :feed_items, dependent: :destroy
 
   validates :feed_url, :group_id, presence: true
   validates :feed_url, uniqueness: { scope: :user_id, message: "should happen once per year" }
