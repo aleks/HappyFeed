b07392c335182e18504ece848f3ac0f3c6f5e202
Aleks <am@klaut.org>
2015-07-05 17:26:51 +0200

    Added E-Mail Validation for User Model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :email, uniqueness: true
+  validate :validate_email
+
   has_many :groups
   has_many :feeds
 
   after_create :add_default_group
 
-  def add_default_group
-    Group.create(user_id: id, title: 'Default Group', default: true)
-  end
+  private
+
+    def add_default_group
+      Group.create(user_id: id, title: 'Default Group', default: true)
+    end
+
+    def validate_email
+      unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+        errors.add(:email, "is not a valid email address!")
+      end
+    end
+
 end
