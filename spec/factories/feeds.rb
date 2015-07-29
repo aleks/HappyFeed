94e766a526b67e67828d7a9f6668a7299a3793d9
Aleks <ihunte@gmail.com>
2015-07-29 17:04:35 +0200

    Feed Factory Changes

--- spec/factories/feeds.rb
+++ spec/factories/feeds.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     user_id 1
     group_id 1
     title "News Site"
-    feed_url "http://happyfeed.tld/feed"
-    site_url "http://happyfeed.tld"
+    feed_url nil
+    site_url nil
     is_spark false
     last_updated_on_time DateTime.now
   end
