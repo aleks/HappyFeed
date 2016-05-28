e42bbcdcdc468fead96d3d8f55eb6b926bae29af
Aleks <am@klaut.org>
2016-05-28 22:02:16 +0200

    Fixes an issue with FeedItems where "cleaned" is not set.

--- db/migrate/20160528193626_add_cleaned_to_feed_item.rb
+++ db/migrate/20160528193626_add_cleaned_to_feed_item.rb
@@ -1,5 +1,7 @@
 class AddCleanedToFeedItem < ActiveRecord::Migration
   def change
     add_column :feed_items, :cleaned, :boolean, default: false
+
+    FeedItem.update_all(cleaned: true)
   end
 end
